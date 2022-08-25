e43a01e3df1dcc726ac50b90257ecdf6cd25e573
Denis Poplavskii <normal64@mail.ru>
2022-08-25 21:05:13 +0200

    aside buildup

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image"
 
 const Layout = (props) => {
   return (
@@ -14,7 +15,13 @@ const Layout = (props) => {
         <meta property="denis dev blog" content="nice guy to hire" />
       </Head>
       <aside className="sidebar">
-          sidebar
+          <h2>timeISlite</h2>
+          <nav>
+              <Link href="/"><a><Image  src="/clock.png" width={32} height={32}></Image></a></Link>
+              <Link href="/log"><a><Image  src="/clock.png" width={32} height={32}></Image></a></Link>
+              <Link href="/summary"><a><Image  src="/clock.png" width={32} height={32}></Image></a></Link>
+          </nav>
+          <div>FAQ</div>
           </aside>
       layoutP{props.children}
     </>
